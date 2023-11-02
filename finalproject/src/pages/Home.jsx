271b953cc520f79cc8a74bfb606b8abd7c6890c3
RaviParmar <raviparmar78822@gmail.com>
2023-11-02 11:52:54 +0530

    footer completed

--- finalproject/src/pages/Home.jsx
+++ finalproject/src/pages/Home.jsx
@@ -11,9 +11,14 @@ import "swiper/css/navigation";
 
 // import required modules
 import { Navigation } from "swiper/modules";
-import { FaEnvelope } from "react-icons/fa6";
+import { FaEnvelope,FaTwitter,FaInstagram,FaPinterestP } from "react-icons/fa6";
+import { BiLogoFacebook } from "react-icons/bi";
+import { TfiYoutube } from "react-icons/tfi";
+
+
 
 import "../css/Home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -258,6 +263,42 @@ const Home = () => {
           exclusive offers, contents, news and other marketing communication as
           a member of the Ray-Ban
         </p>
+        </div>
+        <div className="social-icon" style={{backgroundColor:" "}}>
+          <div className="s-icon">
+         <i><BiLogoFacebook/></i> 
+         <i><FaTwitter/></i>
+          <i><FaInstagram/></i>
+          <i><FaPinterestP/></i>
+          <i><TfiYoutube/></i>
+          </div>
+        </div>
+        <div>
+          <div className="main-footer-bar">
+          <div className="flag-img">
+            <img src="https://img.freepik.com/free-vector/illustration-india-flag_53876-27130.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1698710400&semt=ais" alt="" />
+          </div>
+          <div className="footer-bar">
+            <Link className="f-l">INDIA </Link>
+            <Link className="f-l">How Can we Help </Link>
+            <Link className="f-l">Contact Us</Link>
+            <Link className="f-l">Order Status Inquiry?- </Link>
+          </div>
+          </div>
+          <div className="payment">
+            <div>
+              <h3> WE GUARANTEE EVERY TRANSACTION IS 100% SECURE</h3>
+            </div>
+            <div className="pyment-option">
+              <img src="https://india.ray-ban.com/static/version1698669719/frontend/Aceturtle/Rayban_new/en_US/images/ray_sprite_payment_method.png" alt="" />
+            </div>
+          </div>
+          <div className="p">
+            <p>Pictures and images on this website are for illustration purposes only. No qualities or characteristics of the products depicted herein could be inferred from the relevant pictures. Certain activities undertaken by Luxottica Group S.p.A. may be licensed under US Patent No. 6,624,843. Copyright ©2019 Luxottica Group S.p.A. - All Rights Reserved</p>
+          </div>
+          <div className="copyright" style={{backgroundColor:"#6e716e"}}>
+            <span style={{color:"white" ,fontWeight:"600"}}>Copyright ©2020 Luxottica group. All Rights Reserved</span>
+          </div>
         </div>
       </footer>
     </div>
